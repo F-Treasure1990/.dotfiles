7fe1929293422f0ce6d9d42102fda9a4eb1a83f7
Fenton <fentontreasure@gmail.com>
2025-01-15 21:32:50 +0000

    feat(olm): hyprland,wayland

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -13,6 +13,7 @@ map("n", "<leader>qp", vim.cmd.cprev, { desc = "quickfix next", silent = true })
 -- ==========================================================================
 map("n", "<S-Tab>", "<C-6>", { desc = "Buffer Prev" })
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" }) -- new file
+map("i", "jj", "<esc>", { desc = 'Escape' })
 
 --[[
 local utils = require("core.utils")
@@ -26,14 +27,14 @@ end
 -- ==========================================================================
 map("v", "K", ":m '<-2<CR>gv=gv") -- move highlighted line up
 map("v", "J", ":m '>+1<CR>gv=gv") -- move highlighted line down
-map("n", "J", "mzJ`z") -- move line to end of about line
-map("n", "j", "gj") -- move line to end of about line
-map("n", "k", "gk") -- move line to end of about line
+map("n", "J", "mzJ`z")            -- move line to end of about line
+map("n", "j", "gj")               -- move line to end of about line
+map("n", "k", "gk")               -- move line to end of about line
 
 -- ==========================================================================
 -- Yank
 -- ==========================================================================
-map({ "v", "n" }, "yl", "y$", { desc = "Yank To End Line" }) -- yanks to end of line
+map({ "v", "n" }, "yl", "y$", { desc = "Yank To End Line" })             -- yanks to end of line
 map({ "v", "n" }, "yaf", "<cmd> %y+ <CR>", { desc = "Yank All Buffer" }) -- yank whole file
 
 -- ==========================================================================
@@ -46,7 +47,7 @@ map("n", "<leader>sa", "zg", { desc = "add word" })
 -- ==========================================================================
 -- Vim
 -- ==========================================================================
-map("n", "<leader>vi", vim.show_pos, { desc = "Hl Inspect" }) -- highlight inspect
+map("n", "<leader>vi", vim.show_pos, { desc = "Hl Inspect" })                        -- highlight inspect
 map("n", "<leader>vc", "<cmd>Telescope colorscheme <cr>", { desc = "ColorSchemes" }) -- colorschemes
 map("n", "<leader>vh", "<cmd>Telescope highlights <cr>", { desc = "Highlights" })
 map("n", "<leader>vk", "<cmd>Telescope keymaps<cr>", { desc = "Keymaps" })
