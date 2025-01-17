d8dd224b7d4cb00fb8893fb91521a3205ff1a62c
Fenton <fentontreasure@gmail.com>
2025-01-17 15:27:53 +0000

    feat(cerb): neovim, mini.lua update

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -30,21 +30,23 @@ return {
 
         -- Module mappings. Use `''` (empty string) to disable one.
         mappings = {
-          add = "sa",      -- Add surrounding in Normal and Visual modes
-          delete = "sd",   -- Delete surrounding
-          find = "sf",     -- Find surrounding (to the right)
-          find_left = "sF", -- Find surrounding (to the left)
-          highlight = "sh", -- Highlight surrounding
-          replace = "sc",  -- Replace surrounding
+          add = "sa",            -- Add surrounding in Normal and Visual modes
+          delete = "sd",         -- Delete surrounding
+          find = "sf",           -- Find surrounding (to the right)
+          find_left = "sF",      -- Find surrounding (to the left)
+          highlight = "sh",      -- Highlight surrounding
+          replace = "sc",        -- Replace surrounding
           update_n_lines = "sn", -- Update `n_lines`
 
-          suffix_last = "l", -- Suffix to search with "prev" method
-          suffix_next = "n", -- Suffix to search with "next" method
+          suffix_last = "l",     -- Suffix to search with "prev" method
+          suffix_next = "n",     -- Suffix to search with "next" method
         },
       })
+
       require("mini.cursorword").setup({
         delay = 100,
       })
+
       require("mini.indentscope").setup({
         -- symbol = "▏"┆╎,
         symbol = "▏",
@@ -52,18 +54,10 @@ return {
           try_as_border = true,
         },
       })
+
+      require('mini.statusline').setup({
+        use_icons = true,
+      })
     end,
   },
-  {
-    'echasnovski/mini.nvim',
-    enabled = true,
-    version = '*',
-    opts = {
-      use_icons = true,
-    },
-    config = function(_, opts)
-      local statusline = require 'mini.statusline'
-      statusline.setup(opts)
-    end
-  }
 }
